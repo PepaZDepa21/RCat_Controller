285c15fdfd1260a0e2a406052494122deea6b73e
PepaZDepa21 <honzakovar2110@gmail.com>
2023-06-02 16:30:54 +0200

    Add project files.

---
+++ RCat_Controller/RCat_Controller/Code/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RCat_Controller
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Title = "RCat Controller";
+            BackgroundColor = Color.White;
+            List<Action> actions = new List<Action>() 
+            { 
+                new Action(Guid.NewGuid(), "Action1", "Print"), 
+                new Action(Guid.NewGuid(), "Action2", "Prints"),
+            };
+            ActionsLW.ItemsSource = actions;
+        }
+
+    }
+
+    public class Action
+    {
+        public Guid Id { get; set; }
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        private string description;
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+        public Action() { }
+        public Action(Guid ID, string aName, string aDescription)
+        {
+            Id = ID;
+            Name = aName;
+            Description = aDescription;
+        }
+    }
+}
