75c0af5eaee8de56879c103592663af3639ca752
PepaZDepa21 <92512270+PepaZDepa21@users.noreply.github.com>
2023-06-02 23:08:29 +0200

    Almost finshed

---
+++ RCat_Controller/RCat_Controller/Code/EditAction.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RCat_Controller
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditAction : ContentPage
+    {
+        Action a = new Action();
+        string args = "";
+        int index = 0;
+        public EditAction(Action action, string arg)
+        {
+            InitializeComponent();
+            args = arg;
+            index = Action.AllActions.IndexOf(action);
+            if (arg == "Edit")
+            {
+                a = action.CopyAction();
+                
+            }
+            BindingContext = a;
+        }
+        private async void BtnSaveClicked(object sender, EventArgs e)
+        {
+            if (CheckAction())
+            {
+                if (args == "Edit")
+                {
+                    Action.AllActions[index] = a.CopyAction();
+                    await Navigation.PopAsync();
+                }
+                else
+                {
+                    Action.AllActions.Add(a.CopyAction());
+                    await Navigation.PopAsync();
+                }
+            }
+            else
+            {
+                await DisplayAlert("Unable to save", "Some values are inappropriate!", "Ok");
+            }
+        }
+        public bool CheckAction()
+        {
+            return a.Name != null && a.Name != string.Empty && a.Description != null && a.Description != string.Empty;
+        }
+    }
+}
--- RCat_Controller/RCat_Controller/Code/MainPage.xaml.cs
+++ RCat_Controller/RCat_Controller/Code/MainPage.xaml.cs
@@ -1,40 +1,146 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Essentials;
+using static Xamarin.Essentials.AppleSignInAuthenticator;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Protocol;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
 namespace RCat_Controller
 {
     public partial class MainPage : ContentPage
     {
-        public MainPage()
+        Orientation orientation = new Orientation();
+        public  MainPage()
         {
             InitializeComponent();
             Title = "RCat Controller";
             BackgroundColor = Color.White;
-            List<Action> actions = new List<Action>() 
-            { 
-                new Action(Guid.NewGuid(), "Action1", "Print"), 
-                new Action(Guid.NewGuid(), "Action2", "Prints"),
-            };
-            ActionsLW.ItemsSource = actions;
+            ReadActionsFromFile();
+        }
+        public void UpdateActionsListView()
+        {
+            ActionsLW.ItemsSource = null;
+            ActionsLW.ItemsSource = Action.AllActions;
         }
 
+        public static void WriteActionsToFile()
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Actions.txt")))
+                {
+                    foreach (var item in Action.AllActions)
+                    {
+                        sw.WriteLine(item.SerializeAction());
+                    }
+                    sw.Flush();
+                }
+            }
+            catch (Exception) { }
+        }
+        public void ReadActionsFromFile()
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Actions.txt"));
+                foreach (var item in lines)
+                {
+                    Action.AllActions.Add(Action.DeserializeAction(item));
+                }
+                UpdateActionsListView();
+            }
+            catch (Exception) { }
+        }
+
+
+        private async void BtnAddClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EditAction(new Action(), ""));
+        }
+        private async void BtnActionClicked(object sender, EventArgs e)
+        {
+            Xamarin.Forms.Button button = (Xamarin.Forms.Button)sender;
+            Action a = Action.AllActions[Action.AllActions.IndexOf((Action)button.BindingContext)];
+            if (CBEnableEditing.IsChecked)
+            {
+                await Navigation.PushAsync(new EditAction(a, "Edit"));
+            }
+            else
+            {
+                ConnectAndPublish(a.SerializeAction());
+            }
+        }
+        private void BtnMoveUpClicked(object sender, EventArgs e)
+        {
+            orientation.Movement = 1;
+            ConnectAndPublish(orientation.SerializeOrientation());
+        }
+        private void BtnRotationLeftClicked(object sender, EventArgs e)
+        {
+            orientation.Rotation -= 1;
+            ConnectAndPublish(orientation.SerializeOrientation());
+        }
+        private void BtnRotationRightClicked(object sender, EventArgs e)
+        {
+            orientation.Rotation += 1;
+            ConnectAndPublish(orientation.SerializeOrientation());
+        }
+        private void BtnMoveDownClicked(object sender, EventArgs e)
+        {
+            orientation.Movement = -1;
+            ConnectAndPublish(orientation.SerializeOrientation());
+        }
+        public async void ConnectAndPublish(string textToSend)
+        {
+            var factory = new MqttFactory();
+            var client = factory.CreateMqttClient();
+
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer("test.mosquitto.org", 1883)
+                .Build();
+
+            await client.ConnectAsync(options);
+
+            // Publish a message
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic("RCat/Pepa")
+                .WithPayload(textToSend)
+                .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+                .Build();
+
+            await client.PublishAsync(message);
+
+            await client.DisconnectAsync();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            WriteActionsToFile();
+            UpdateActionsListView();
+        }
     }
 
     public class Action
     {
+        [JsonIgnore]
+        public static List<Action> AllActions = new List<Action>();
         public Guid Id { get; set; }
         private string name;
         public string Name
         {
-            get
-            {
-                return name;
-            }
+            get => name;
             set
             {
                 name = value;
@@ -43,21 +149,56 @@ namespace RCat_Controller
         private string description;
         public string Description
         {
-            get
-            {
-                return description;
-            }
+            get => description;
             set
             {
                 description = value;
             }
         }
-        public Action() { }
+        public Action() { Id = Guid.NewGuid(); }
         public Action(Guid ID, string aName, string aDescription)
         {
             Id = ID;
             Name = aName;
             Description = aDescription;
         }
+        public Action CopyAction() => new Action(Id, Name, Description);
+        public string SerializeAction() => JsonSerializer.Serialize(this);
+        public static Action DeserializeAction(string json) 
+        {
+            Action a = JsonSerializer.Deserialize<Action>(json);
+            a.Id = Guid.NewGuid();
+            return a.CopyAction();
+        }
+    }
+    class Orientation
+    {
+        private int rotation;
+        public int Rotation { 
+            get => rotation; 
+            set 
+            {
+                if (value == 360)
+                {
+                    rotation = 0;
+                }
+                else if (value == -1)
+                {
+                    rotation = 359;
+                }
+                else
+                {
+                    rotation = value;
+                }
+                Movement = 0;
+            } 
+        }
+
+        private int movement;
+        public int Movement { get { return movement; } set {  movement = value; } }
+
+        public Orientation() { rotation = 0; Movement = 0; }
+        
+        public string SerializeOrientation() => JsonSerializer.Serialize(this);
     }
 }
